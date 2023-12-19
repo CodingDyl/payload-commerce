c9d31ef07e41b1dd8070b79a019c95986717c5f4
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2023-12-19 10:20:38 +0200

    completed footer

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,32 +1,38 @@
 'use client';
 
 import React from 'react'
-import { Footer } from '../../../../payload/payload-types'
+import { Footer, Media } from '../../../../payload/payload-types'
 
 import classes from './index.module.scss'
-import { inclusions, noHeaderFooterUrls } from '../../../constants'
+import { inclusions, noHeaderFooterUrls,  } from '../../../constants'
 import { usePathname } from 'next/navigation'
 import { Gutter } from '../../Gutter';
-import Image from 'next/image';
 import Link from 'next/link';
+import Image from 'next/image';
+import { Button } from '../../Button';
 
 const FooterComponent = ({footer}: {footer: Footer}) => {
     const pathname = usePathname();
+    const navItems = footer?.navItems || []
   return (
     <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
         <Gutter>
-            <ul className={classes.inclusions}>
-                {
-                    inclusions.map((inclusion, index) => (
-                       <li key={inclusion.title}>
-                        <Image src={inclusion.icon} alt='inclusion.title' width={36} height={36} className={classes.icon}/>
+        <ul className={classes.inclusions}>
+          {inclusions.map(inclusion => (
+            <li key={inclusion.title}>
+              <Image
+                src={inclusion.icon}
+                alt={inclusion.title}
+                width={36}
+                height={36}
+                className={classes.icon}
+              />
 
-                        <h5 className={classes.title}>{inclusion.title}</h5>
-                        <p>{inclusion.description}</p>
-                       </li> 
-                    ))
-                }
-            </ul>
+              <h5 className={classes.title}>{inclusion.title}</h5>
+              <p>{inclusion.description}</p>
+            </li>
+          ))}
+        </ul>
         </Gutter>
 
         <div className={classes.footer}>
@@ -36,7 +42,25 @@ const FooterComponent = ({footer}: {footer: Footer}) => {
                     <Image src='/logo-white.svg' alt='logo' width={170} height={50}/>
                   </Link>
 
-                  {/* <p>{footer.copyright}</p> */}
+                  <p>{footer.copyright}</p>
+
+                  <div className={classes.socialLinks}>
+                    {navItems.map((item) => {
+                      const icon = item?.link?.icon as Media;
+
+                      return (
+                        <Button 
+                          key={item.link.label}
+                          el="link"
+                          href={item.link.url}
+                          newTab={true}
+                          className={classes.socialLinkItem}
+                        >
+                          <Image src={icon?.url} alt={item.link.label} width={24} height={24} className={classes.socailIcon}/>
+                        </Button>
+                      )
+                      })}
+                  </div>
                 </div>
             </Gutter>
         </div>
